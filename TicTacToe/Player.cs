46ced66bfd7978799a651ae53c73cbe50a8789cb
George Xiros <geo.xiros@gmail.com>
2018-11-04 20:36:11 +0200

    Change WinCombinations to List<byte[]>

--- TicTacToe/Player.cs
+++ TicTacToe/Player.cs
@@ -11,16 +11,16 @@ namespace TicTacToe
     public char PlayerLetter { get; private set; }
     private byte _Tile;
     public byte Tile { get { return (byte)(_Tile - 1); } protected set { _Tile = value; } }
-    readonly List<List<byte>>[] WinCombinations = {
-            new List<List<byte>> { new List<byte> { 1, 2 }, new List<byte> { 3, 6 }, new List<byte> { 4, 8 } },
-            new List<List<byte>> { new List<byte> { 0, 2 }, new List<byte> { 4, 7 } },
-            new List<List<byte>> { new List<byte> { 0, 1 }, new List<byte> { 4, 6 }, new List<byte> { 5, 8 } },
-            new List<List<byte>> { new List<byte> { 0, 6 }, new List<byte> { 4, 5 }, },
-            new List<List<byte>> { new List<byte> { 0, 8 }, new List<byte> { 1, 7 }, new List<byte> { 2, 6 }, new List<byte> { 3, 5 } },
-            new List<List<byte>> { new List<byte> { 2, 8 }, new List<byte> { 3, 4 }, },
-            new List<List<byte>> { new List<byte> { 0, 3 }, new List<byte> { 7, 8 }, new List<byte> { 4, 2 } },
-            new List<List<byte>> { new List<byte> { 6, 8 }, new List<byte> { 1, 4 } },
-            new List<List<byte>> { new List<byte> { 6, 7 }, new List<byte> { 2, 5 }, new List<byte> { 0, 4 } }};
+    readonly List<byte[]>[] WinCombinations = {
+            new List<byte[]> { new byte[] { 1, 2 }, new byte[] { 3, 6 }, new byte[] { 4, 8 } },
+            new List<byte[]> { new byte[] { 0, 2 }, new byte[] { 4, 7 } },
+            new List<byte[]> { new byte[] { 0, 1 }, new byte[] { 4, 6 }, new byte[] { 5, 8 } },
+            new List<byte[]> { new byte[] { 0, 6 }, new byte[] { 4, 5 }, },
+            new List<byte[]> { new byte[] { 0, 8 }, new byte[] { 1, 7 }, new byte[] { 2, 6 }, new byte[] { 3, 5 } },
+            new List<byte[]> { new byte[] { 2, 8 }, new byte[] { 3, 4 }, },
+            new List<byte[]> { new byte[] { 0, 3 }, new byte[] { 7, 8 }, new byte[] { 4, 2 } },
+            new List<byte[]> { new byte[] { 6, 8 }, new byte[] { 1, 4 } },
+            new List<byte[]> { new byte[] { 6, 7 }, new byte[] { 2, 5 }, new byte[] { 0, 4 } }};
 
     public Player(String name, PlayerLetter playerLetter)
     {
@@ -35,7 +35,7 @@ namespace TicTacToe
     public bool DoesPlayerWins(Board board, byte Tile, char PlayerLetter)
     {
       // check all cobinations for winning
-      foreach (List<byte> Combination in WinCombinations[Tile])
+      foreach (byte[] Combination in WinCombinations[Tile])
       {
         // If Has player letter in both win combinations positions then player wins
         if ((board.GetTileValue(Combination[0]) == PlayerLetter) &&
